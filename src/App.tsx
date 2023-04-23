e57a266a297f01cbd47520d14fc5b671355bc9a3
Domingos Costa <domingos.costa@wiremaze.com>
2023-04-23 22:48:03 +0100

    Start of nestjs app

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import React, { useState } from 'react'
 import './App.css';
 
 import fetchData from "./api/OpenAPIService";
-import useSelectedText from "./utils/Utils";
-import { text } from 'stream/consumers';
 
 function App() {
   const [completedSentence, setCompletedSentence] = useState("");
 
   async function useHandleClick() {
     try {
-      let textToSummarize : string | undefined = undefined;
       chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
         function(tabs) {
-          console.log("This is the tab" + tabs[0]);
           chrome.scripting.executeScript({
             target: {tabId: tabs[0].id!, allFrames: true},
             func: () => {
@@ -21,9 +17,7 @@ function App() {
             }
           }).then(injectionResults => {
             for (const {result} of injectionResults) {
-              textToSummarize = result;
-              console.log("This is the result" + result);
-              fetchData(textToSummarize!).then(completedSentence => {
+              fetchData(result!).then(completedSentence => {
                 setCompletedSentence(completedSentence!);
               });
             }
@@ -37,8 +31,8 @@ function App() {
   return (
     <div className="container">
       <h2>Text Summarizer</h2>
-      <button className="button" onClick={useHandleClick}>Complete Sentence</button>
-      {completedSentence && <p>Completed sentence: {completedSentence}</p>}
+      <button className="button" onClick={useHandleClick}>Summarize</button>
+      {completedSentence && <p>Summarized sentence: {completedSentence}</p>}
     </div>
     
   );
